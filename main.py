8b5d3427f177829ed388aa8dca2236e894df301f
Looty <ayeksvadr.best@bk.ru>
2025-07-23 11:33:56 +0500

    Создание базовой структуры класса

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # calculator_app.py
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Простой Калькулятор")
-root.mainloop()
+class CalculatorApp:
+   def __init__(self, master):
+      self.master = master
+      master.title("Калькулятор")
+      self.display = tk.Entry(master, width=25, borderwidth=5, relief="sunken", font=('Arial', 16), justify="left")
+      self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+      self.display.insert(0, "gaysex")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    my_calculator = CalculatorApp(root)
+    root.mainloop()      
